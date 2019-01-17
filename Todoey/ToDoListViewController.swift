d7addbada76d162a1cd9eb7c46458236add42e19
Mantas Skeiverys <mantasskeiverys@imac-3.lan>
2019-01-17 12:32:28 +0200

    Added test comment

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -28,6 +28,7 @@ class ToDoListViewController: UITableViewController {
         return cell
     }
     
+    //test comment
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
